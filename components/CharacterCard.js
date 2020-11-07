8e67927b15126579465ead36c7d6dea94f7c1725
Nayely Rodarte <nayelyrodarte@gmail.com>
2020-11-06 18:45:39 -0600

    Update character card styling

--- components/CharacterCard.js
+++ components/CharacterCard.js
@@ -1,12 +1,38 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+
+const cardStyles = makeStyles({
+  root: {
+    backgroundColor: 'black',
+    borderRadius: '20px',
+    color: 'white',
+    display: 'grid',
+    fontFamily: 'Roboto',
+    fontSize: '0.8em',
+    lineHeight: '1px',
+    padding: '0.2em',
+    textAlign: 'center',
+  },
+  img: {
+    borderRadius: '50%',
+    margin: 'auto',
+    marginTop: '1em',
+    width: '10em',
+  },
+  title: {
+    fontSize: '1.2em',
+    fontWeight: 'bold',
+  },
+});
 
 function CharacterCard({ character }) {
+  const styles = cardStyles();
   const { name, image, status, species, location } = character;
 
   return (
-    <div>
-      <img src={image} alt={`${name} avatar`} />
-      <p>{name}</p>
+    <div className={styles.root}>
+      <img src={image} alt={`${name} avatar`} className={styles.img} />
+      <p className={styles.title}>{name}</p>
       <p>{status}</p>
       <p>{species}</p>
       <p>{location.name}</p>
